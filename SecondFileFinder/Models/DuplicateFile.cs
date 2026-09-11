4e94ee026cedc8d7d1471707aa20893838d055fb
Kalhara Nonis <kalhara.nonis.0@gmail.com>
2026-09-11 22:36:20 +0530

    Add files via upload

---
+++ SecondFileFinder/Models/DuplicateFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecondFileFinder.Models
+{
+    internal class DuplicateFile
+    {
+        public string Filename { get; set; }
+        public string Folder { get; set; }
+        public long Size { get; set; }
+        public string Hash { get; set; }
+    }
+}
