544ab5dc53be6c8ff0609777dba0605f2c03d6ff
Kalhara Nonis <kalhara.nonis.0@gmail.com>
2026-09-11 22:29:56 +0530

    Add files via upload

---
+++ SecondFileFinder/Services/FileScanner.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SecondFileFinder.Models;
+using System.Security.Cryptography;
+
+namespace SecondFileFinder.Services
+{
+    internal class FileScanner
+    {
+        public async Task< List<DuplicateFile>> Scan(string folder,IProgress<int> progress) {
+
+            Dictionary<long,List<string>> sizeGroups= new Dictionary<long,List<string>>();
+            Dictionary<string,List<string>> hashGroups= new Dictionary<string,List<string>>();
+
+            List<DuplicateFile> duplicates = new List<DuplicateFile>();
+
+            int count = 0;
+
+            foreach (string file in Directory.EnumerateFiles(folder,"*.*",SearchOption.AllDirectories)) {
+                try
+                {
+                    FileInfo info = new FileInfo(file);
+
+                    if (!sizeGroups.ContainsKey(info.Length))
+                        sizeGroups[info.Length] = new List<string>();
+                    sizeGroups[info.Length].Add(file);
+
+                    count++;
+                    progress?.Report(count);
+                    await Task.Yield();
+                }
+                catch { continue; }
+            }
+
+            foreach (var group in sizeGroups) {
+                if (group.Value.Count < 2)
+                    continue;
+                foreach (string file in group.Value) {
+                    string hash = HashService.GetFileHash(file);
+
+                    if (!hashGroups.ContainsKey(hash)) 
+                        hashGroups[hash]= new List<string>();
+
+                    hashGroups[hash].Add(file);
+                    
+                }
+            }
+
+            foreach (var group in hashGroups) {
+                if (group.Value.Count < 2)
+                    continue;
+                foreach (string file in group.Value) {
+                    FileInfo info = new FileInfo(file);
+                    duplicates.Add(new DuplicateFile
+                    {
+                        Filename = info.Name,
+                        Folder = info.DirectoryName,
+                        Size = info.Length,
+                        Hash=group.Key
+                    });
+                }
+            }
+            return duplicates;
+        }
+    }
+}
---
+++ SecondFileFinder/Services/HashService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SecondFileFinder.Services
+{
+    public class HashService
+    {
+         public static string GetFileHash(string filePath) {
+            using (FileStream stream = File.OpenRead(filePath))
+            using (SHA256 sHA = SHA256.Create()) {
+                byte[] hash = sHA.ComputeHash(stream);
+                StringBuilder stb = new StringBuilder();
+                foreach (byte b in hash)
+                {
+                    stb.Append(b.ToString("x2"));
+                }
+                return stb.ToString();
+            }
+        }
+    }
+}
